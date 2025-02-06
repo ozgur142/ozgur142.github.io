8cc8a0bf14acde4d5ae5fd32b995745abb645abf
Ozgur <ozgurcandogan@gmail.com>
2025-02-06 14:13:25 +0100

    mobil compatiblity added with scrolable projects

--- js/navbar.js
+++ js/navbar.js
@@ -3,3 +3,10 @@ document.getElementById('menu-btn').addEventListener('click', function() {
     mobileMenu.classList.toggle('hidden');
 });
 
+// scroll event listener to hide mobile menu on scroll
+window.addEventListener('scroll', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (!mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.add('hidden');
+    }
+});
